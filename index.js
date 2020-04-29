ff590f2ee91c2c7cb5feb589044d18ff29078028
Ben-Mustion <benmustion@gmail.com>
2020-04-29 10:46:03 -0700

    setup auth routes - bcrypt - jwt

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const app = express();
+const router = require('./router');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/auth', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+app.use(morgan('combined'));
+app.use(bodyParser.json({ type: '*/*' }));
+router(app);
+
+const port = process.env.PORT || 3090;
+app.listen(port, () => {
+  console.log(`Server listening on:`, port);
+});
